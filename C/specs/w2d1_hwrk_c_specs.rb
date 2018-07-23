934f4f99bedc922fbdda18f573ebefedf7a58780
fddata <fraserjdouglas@gmail.com>
2018-07-23 21:18:40 +0100

    initial commit, adds test_get_books pass

---
+++ C/specs/w2d1_hwrk_c_specs.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../w2d1_hwrk_c.rb'
+
+class LibraryTest < MiniTest::Test
+
+  def setup
+
+    #Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date.
+
+    @books1 =Library.new(
+      [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/18"
+        }
+      },
+      {
+        title: "the_two_towers",
+        rental_details: {
+          student_name: "simon",
+          date: "11/12/18"
+        }
+      }
+    ])
+
+  end
+
+
+#Create a getter for the books
+ def test_get_books
+   result = @books1.books
+   assert_equal(2, result.length)
+ end
+
+#Create a method that takes in a book title and returns all of the information about that book.
+# def test_get_info
+#
+#   result = @books1.get_info("the_two_towers")
+#   assert_equal(4, result.size)
+# end
+
+
+
+end
---
+++ C/w2d1_hwrk_c.rb
@@ -0,0 +1,21 @@
+
+#Model a Library as a class.
+
+class Library
+
+  attr_accessor :books
+
+  def initialize(books)
+    @books = books
+  end
+
+
+
+
+
+end
+
+
+# Create a method that takes in a book title and returns only the rental details for that book.
+# Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
+# Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it's due to be returned.
