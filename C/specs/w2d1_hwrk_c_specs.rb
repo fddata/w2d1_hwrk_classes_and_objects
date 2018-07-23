75431fc939a443fb3ec6b3b1cacc04d2907b7953
fddata <fraserjdouglas@gmail.com>
2018-07-23 22:37:24 +0100

    extentions up to last two exercises

--- C/specs/w2d1_hwrk_c_specs.rb
+++ C/specs/w2d1_hwrk_c_specs.rb
@@ -10,42 +10,74 @@ class LibraryTest < MiniTest::Test
 
     @books1 =Library.new(
       [
-      {
-        title: "lord_of_the_rings",
-        rental_details: {
-          student_name: "Jeff",
-          date: "01/12/18"
+        {
+          title: "lord_of_the_rings",
+          rental_details: {
+            student_name: "Jeff",
+            date: "01/12/18"
+          }
+        },
+        {
+          title: "the_two_towers",
+          rental_details: {
+            student_name: "simon",
+            date: "11/12/18"
+          }
         }
-      },
-      {
-        title: "the_two_towers",
-        rental_details: {
-          student_name: "simon",
-          date: "11/12/18"
-        }
-      }
-    ])
+        ])
+        @books2 =Library.new(
+          [
+            {
+              title: "lord_of_the_flies",
+              rental_details: {
+                student_name: "",
+                date: ""
+              }
+            },
+            {
+              title: "the_two_guys",
+              rental_details: {
+                student_name: "",
+                date: ""
+              }
+            }
+            ])
+    end
+
+
+
+
+    #Create a getter for the books
+    def test_get_books
+      result = @books1.books
+      assert_equal(2, result.length)
+    end
+
+    #Create a method that takes in a book title and returns all of the information about that book.
+    def test_get_info
+
+      result = @books1.get_info("the_two_towers")
+      assert_equal(true, result[:rental_details][:student_name] == "simon")
+    end
+
+    # Create a method that takes in a book title and returns only the rental details for that book.
+
+    def test_get_rental_details
+      result = @books1.get_rental_details("the_two_towers")
+      assert_equal(true, result[:student_name] == "simon")
+
+    end
+
+    # Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
+
+    # def test_add_book
+    #   @books1.add_book(@book12
+    #   #assert_equal(3, @books1.count)
+    #   p @books1
+    #   p @books2
+    #
+    # end
+
+
 
   end
-
-
-#Create a getter for the books
- def test_get_books
-   result = @books1.books
-   assert_equal(2, result.length)
- end
-
-#Create a method that takes in a book title and returns all of the information about that book.
- def test_get_info
-
-   result = @books1.get_info("the_two_towers")
-   assert_equal(2, result.length)
- end
-
-#   result = @books1.get_info("the_two_towers")
-#   assert_equal(4, result.size)
-# end
-
-
-
-end
--- C/w2d1_hwrk_c.rb
+++ C/w2d1_hwrk_c.rb
@@ -13,13 +13,16 @@ class Library
     @books.select {|book| book[:title] == name }[0]
   end
 
+  def get_rental_details(name)
+    return get_info(name)[:rental_details]
 
+  end
 
-
+ def add_book(new_book)
+   @books.push(new_book)
+ end
 
 end
 
 
-# Create a method that takes in a book title and returns only the rental details for that book.
-# Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
 # Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it's due to be returned.
